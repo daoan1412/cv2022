e600a5ab53e3c53b3df5952d8c3dc6584adc6352
Dao An <daoan1412@gmail.com>
2022-02-04 22:23:38 +0700

    add docs folder

--- out/_next/static/2aWXmtEc8CwpKcZcmKURk/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b6a949d7370ce9b1.js"],"/_error":["static/chunks/pages/_error-7dc2ab31b1753efa.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
