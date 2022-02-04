7787940ba1a049a4ee9b64102389f895b71b4802
Dao An <daoan1412@gmail.com>
2022-02-04 22:38:09 +0700

    fix bugs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
+
 const nextConfig = {
   reactStrictMode: true,
   optimizeFonts: false,
-  swcMinify: false
+  swcMinify: false,
+  assetPrefix: isProd ? 'https://raw.githubusercontent.com/daoan1412/cv2022/main/docs' : '',
 }
 
 module.exports = nextConfig
